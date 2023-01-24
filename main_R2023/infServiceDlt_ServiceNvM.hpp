0b1e21fa8c4ee6785978e26a915939ef7601538d
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:50:44 +0100

    Update: AUTOSAR basic software service module for diagnostic logging and tracing

---
+++ main_R2023/infServiceDlt_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infServiceDlt_ServiceNvM.hpp                                      */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTSERVICEDLT_AR_RELEASE_VERSION_MAJOR                               4
+#define CONSTSERVICEDLT_AR_RELEASE_VERSION_MINOR                               3
+
+#define Const_DefaultInit_ConstServiceDlt                                                                              \
+   ,  {CONSTSERVICEDLT_AR_RELEASE_VERSION_MAJOR, CONSTSERVICEDLT_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
