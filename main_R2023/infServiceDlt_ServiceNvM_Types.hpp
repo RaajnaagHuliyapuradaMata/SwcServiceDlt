e92e97e297177f2ca8c0ce59215694b4c584a190
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-02-04 19:29:33 +0100

    Update: AUTOSAR basic software service module for diagnostic logging and tracing

--- main_R2023/infServiceDlt_ServiceNvM_Types.hpp
+++ main_R2023/infServiceDlt_ServiceNvM_Types.hpp
@@ -10,9 +10,11 @@
 class NvM_BlocksRom_ServiceDlt_Type:
       public ConstModule_TypeAbstract
 {
-/******************************************************************************/
-/* CONSTS                                                                     */
-/******************************************************************************/
+};
+
+class NvM_BlocksNv_ServiceDlt_Type:
+      public CfgModule_TypeAbstract
+{
 };
 
 /******************************************************************************/
